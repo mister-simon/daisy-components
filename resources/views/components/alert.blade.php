06a4dd3109c6eaa8f008ead734cec281dc114d02
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-03 12:15:24 +0100

    Cleaning up optional slot checks

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,6 +1,10 @@
+@php($action ??= null)
+@php($icon ??= null)
 <div {{ $attributes->merge($defaultAttributes) }}>
-    @if ($info)
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="h-6 w-6 shrink-0 stroke-current">
+    @if ($icon)
+        {{ $icon }}
+    @elseif ($info)
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
         </svg>
     @elseif ($success)
@@ -21,11 +25,11 @@
 
     <div>{{ $slot }}</div>
 
-    @if ($action ?? null)
+    @if ($action)
         {{ $action }}
     @endif
 
-    @if ($dismissable && !($action ?? null))
+    @if ($dismissable && $action === null)
         <x-daisy-components::button circle ghost sm x-on:click="dismissed = true">✕</x-daisy-components::button>
     @endif
 </div>
