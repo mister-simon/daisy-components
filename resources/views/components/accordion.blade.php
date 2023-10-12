329ea2714b4e6d0fef3302893bcc2eba300a8c4e
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-12 11:48:35 +0100

    Allow accordion contents to be passed via slot.

--- resources/views/components/accordion.blade.php
+++ resources/views/components/accordion.blade.php
@@ -1,5 +1,9 @@
 <div {{ $attributes->merge($defaultAttributes) }}>
     <input type="radio" name="{{ $group }}" @checked($open) />
     <div {{ $title->attributes->class(['collapse-title']) }}>{{ $title }}</div>
-    <div {{ $content->attributes->class(['collapse-content']) }}>{{ $content }}</div>
+    @isset($content)
+        <div {{ $content->attributes->class(['collapse-content']) }}>{{ $content }}</div>
+    @else
+        <div @class(['collapse-content'])>{{ $slot }}</div>
+    @endisset
 </div>
