c67018c347eaf8f4b154d878467fdc3a624f3e92
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-17 16:31:21 +0100

    Add missing functionality to label

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,1 +1,19 @@
-<label {{ $attributes->class('label') }}>{{ $slot }}</label>
+@php($text ??= $attributes->get('text'))
+@php($textAlt ??= $attributes->get('text-alt'))
+<label {{ $attributes->class('label')->except('text') }}>
+    @if ($text !== null)
+        @if (is_string($text))
+            <div class="label-text">{{ $text }}</div>
+        @else
+            <div {{ $text->attributes->class('label-text') }}>{{ $text }}</div>
+        @endif
+    @endif
+    {{ $slot }}
+    @if ($textAlt !== null)
+        @if (is_string($textAlt))
+            <div class="label-text-alt">{{ $textAlt }}</div>
+        @else
+            <div {{ $textAlt->attributes->class('label-text') }}>{{ $textAlt }}</div>
+        @endif
+    @endif
+</label>
