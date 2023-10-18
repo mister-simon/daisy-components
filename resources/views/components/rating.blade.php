64e29a0e89667c6df8a1fbe346ed8c191e522694
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-18 15:00:29 +0100

    Add support for x-model on rating

--- resources/views/components/rating.blade.php
+++ resources/views/components/rating.blade.php
@@ -3,14 +3,15 @@
 @php($start = $half ? 0.5 : 1)
 @php($name ??= $attributes->get('name', 'rating'))
 @php($ratingClasses = \Illuminate\Support\Arr::wrap($ratingClasses))
-<div {{ $attributes->merge($defaultAttributes)->except(['name']) }}>
+<div {{ $attributes->merge($defaultAttributes)->except(['name', 'x-model']) }}>
     @if ($hidden)
         <input
             type="radio"
             name="{{ $name }}"
             value="0"
             class="rating-hidden"
-            @checked($checked === null || (int) $checked === 0) />
+            @checked($checked === null || (int) $checked === 0)
+            {{ $attributes->only('x-model') }} />
     @endif
     @if ($slot->isEmpty())
         @for ($i = $start; $i < $max + $step; $i += $step)
@@ -24,7 +25,8 @@
                     'mask-half-1' => $half && $isOdd,
                     'mask-half-2' => $half && !$isOdd,
                 ])
-                @checked((float) $checked === (float) $i) />
+                @checked((float) $checked === (float) $i)
+                {{ $attributes->only('x-model') }} />
         @endfor
     @else
         {{ $slot }}
