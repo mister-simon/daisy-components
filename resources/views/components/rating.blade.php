59f9b72fdbc97625fa17e6164372580ac3682df2
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-19 11:47:15 +0100

    Updates rating to make use of inner rating slot for alpine / styling purposes.

--- resources/views/components/rating.blade.php
+++ resources/views/components/rating.blade.php
@@ -1,9 +1,8 @@
 @php($count = $half ? $max * 2 : $max)
 @php($step = $half ? 0.5 : 1)
-@php($start = $half ? 0.5 : 1)
-@php($name ??= $attributes->get('name', 'rating'))
 @php($ratingClasses = \Illuminate\Support\Arr::wrap($ratingClasses))
-<div {{ $attributes->merge($defaultAttributes)->except(['name', 'x-model']) }}>
+@php($rating ??= new Illuminate\View\ComponentSlot(attributes: ['name' => $name]))
+<div {{ $attributes->merge($defaultAttributes)->except(['x-model']) }}>
     @if ($hidden)
         <input
             type="radio"
@@ -11,22 +10,21 @@
             value="0"
             class="rating-hidden"
             @checked($checked === null || (int) $checked === 0)
-            {{ $attributes->only('x-model') }} />
+            {{ $rating->attributes->merge([
+                    'x-model' => $attributes->get('x-model'),
+                ])->only('x-model') }} />
     @endif
     @if ($slot->isEmpty())
-        @for ($i = $start; $i < $max + $step; $i += $step)
+        @for ($i = $step; $i < $max + $step; $i += $step)
             @php($isOdd = ($i / $step) % 2)
-            <input
-                type="radio"
-                name="{{ $name }}"
-                value="{{ $i }}"
-                @class([
-                    ...$ratingClasses,
-                    'mask-half-1' => $half && $isOdd,
-                    'mask-half-2' => $half && !$isOdd,
-                ])
-                @checked((float) $checked === (float) $i)
-                {{ $attributes->only('x-model') }} />
+            <input {{ $rating->attributes->class([...$ratingClasses, 'mask-half-1' => $half && $isOdd, 'mask-half-2' => $half && !$isOdd])->merge([
+                    'x-model' => $attributes->get('x-model'),
+                ])->merge([
+                    'type' => 'radio',
+                    'name' => $name,
+                    'value' => $i,
+                ])->except(['checked']) }}
+                @checked((float) $checked === (float) $i) />
         @endfor
     @else
         {{ $slot }}
