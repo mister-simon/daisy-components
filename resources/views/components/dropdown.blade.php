6ecffee9d66866c8d4b7d4cea487ee555ead0b84
Simon W <4751279+mister-simon@users.noreply.github.com>
2023-10-20 11:02:22 +0100

    Removes presumption that dropdowns are always activated by button components. Adds support for default slot content.

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,4 +1,5 @@
+@php($content ??= new Illuminate\View\ComponentSlot($slot->toHtml()))
 <div {{ $attributes->merge($defaultAttributes) }}>
-    <label {{ $summary->attributes->class('btn')->merge(['tabindex' => '0']) }}>{{ $summary }}</label>
+    <label {{ $summary->attributes->merge(['tabindex' => '0']) }}>{{ $summary }}</label>
     <div {{ $content->attributes->class(['dropdown-content z-[1]']) }}>{{ $content }}</div>
 </div>
